865ce20cc27b92d48d16a8168d88c8d2e2da34a8
Gideon Juve <juve@usc.edu>
2008-05-29 15:15:50 +0000

    Update database fields Drop proxy columns Remove glidein_environment; we will use site_environment for both Rename site_environment to environment Add lastUpdate and submitted to both site and glidein
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1217 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- schema/create-mysql.sql
+++ schema/create-mysql.sql
@@ -6,52 +6,48 @@ CREATE TABLE site (
 	submitPath		TEXT NOT NULL,
 	condorPackage	VARCHAR(1024),
 	condorVersion	VARCHAR(16),
-	status			VARCHAR(16),
-	statusMessage	VARCHAR(1024),
-	CONSTRAINT		pk_site PRIMARY KEY (id)
+	status			VARCHAR(16) NOT NULL,
+	statusMessage	VARCHAR(1024) NOT NULL,
+	submitted		DATETIME NOT NULL,
+	lastUpdate		DATETIME NOT NULL,
+	CONSTRAINT pk_site PRIMARY KEY (id)
 ) type=InnoDB;
 
-CREATE TABLE site_environment (
+CREATE TABLE environment (
 	site			BIGINT NOT NULL,
 	variable		VARCHAR(255) NOT NULL,
 	value			TEXT,
-	CONSTRAINT		pk_site_environment PRIMARY KEY (site,variable),
-	CONSTRAINT		fk_site_environment_01 FOREIGN KEY (site) REFERENCES site(id) ON DELETE CASCADE
+	CONSTRAINT pk_environment PRIMARY KEY (site,variable),
+	CONSTRAINT fk_environment_01 FOREIGN KEY (site) REFERENCES site(id) ON DELETE CASCADE
 ) type=InnoDB;
 
 CREATE TABLE execution_service (
 	site			BIGINT NOT NULL,
 	function		ENUM('STAGING','GLIDEIN') NOT NULL,
-	serviceContact	VARCHAR(1024),
+	serviceContact	VARCHAR(1024) NOT NULL,
 	serviceType		ENUM('GT2','GT4') NOT NULL,
 	project			VARCHAR(255),
 	queue			VARCHAR(255),
-	proxy			TEXT NOT NULL,
-	CONSTRAINT		pk_execution_service PRIMARY KEY (site,function),
-	CONSTRAINT		fk_execution_service_01 FOREIGN KEY (site) REFERENCES site(id) ON DELETE CASCADE
+	CONSTRAINT pk_execution_service PRIMARY KEY (site,function),
+	CONSTRAINT fk_execution_service_01 FOREIGN KEY (site) REFERENCES site(id) ON DELETE CASCADE
 ) type=InnoDB;
 
 CREATE TABLE glidein (
-	id				BIGINT NOT NULL AUTO_INCREMENT,
-	site			BIGINT NOT NULL,
-	count			INTEGER,
-	hostCount		INTEGER,
-	wallTime		INTEGER,
-	numCpus			INTEGER,
-	configBase64	TEXT,
-	gcbBroker		VARCHAR(15),
-	idleTime		INTEGER,
-	debug			TEXT,
-	status			VARCHAR(16),
-	statusMessage	VARCHAR(1024),
-	CONSTRAINT		pk_glidein PRIMARY KEY (id),
-	CONSTRAINT		fk_glidein_01 FOREIGN KEY (site) REFERENCES site(id) ON DELETE CASCADE
-) type=InnoDB;
-
-CREATE TABLE glidein_environment (
-	glidein			BIGINT NOT NULL,
-	variable		VARCHAR(255) NOT NULL,
-	value			TEXT,
-	CONSTRAINT		pk_glidein_environment PRIMARY KEY (glidein,variable),
-	CONSTRAINT		fk_glidein_environment_01 FOREIGN KEY (glidein) REFERENCES glidein(id) ON DELETE CASCADE
+	id					BIGINT NOT NULL AUTO_INCREMENT,
+	site				BIGINT NOT NULL,
+	condorHost			VARCHAR(256) NOT NULL,
+	count				INTEGER NOT NULL,
+	hostCount			INTEGER NOT NULL,
+	wallTime			INTEGER NOT NULL,
+	numCpus				INTEGER NOT NULL,
+	condorConfigBase64	TEXT,
+	gcbBroker			VARCHAR(15),
+	idleTime			INTEGER,
+	condorDebug			TEXT,
+	status				VARCHAR(16) NOT NULL,
+	statusMessage		VARCHAR(1024) NOT NULL,
+	submitted			DATETIME NOT NULL,
+	lastUpdate			DATETIME NOT NULL,
+	CONSTRAINT pk_glidein PRIMARY KEY (id),
+	CONSTRAINT fk_glidein_01 FOREIGN KEY (site) REFERENCES site(id) ON DELETE CASCADE
 ) type=InnoDB;
--- schema/drop-mysql.sql
+++ schema/drop-mysql.sql
@@ -1,5 +1,4 @@
 drop table if exists execution_service;
-drop table if exists glidein_environment;
 drop table if exists glidein;
-drop table if exists site_environment;
+drop table if exists environment;
 drop table if exists site;
