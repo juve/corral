cf2937b5a276725b6106cb56a29bfd70734d577f
Gideon Juve <juve@usc.edu>
2008-05-29 15:43:14 +0000

    Rename GlideinQNames to GlideinNames Rename SiteQNames to SiteNames
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1246 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/service/impl/GlideinNames.java
+++ src/edu/usc/glidein/service/impl/GlideinNames.java
@@ -2,11 +2,13 @@ package edu.usc.glidein.service.impl;
 
 import javax.xml.namespace.QName;
 
-public class GlideinQNames
+public class GlideinNames
 {
 	public static final String SERVICE_NS = "http://www.usc.edu/glidein/GlideinService";
 	public static final QName RESOURCE_KEY = new QName(SERVICE_NS, "GlideinServiceKey");
 	public static final QName RESOURCE_PROPERTIES = new QName(SERVICE_NS, "GlideinResourceProperties");
 	
 	public static final QName RP_GLIDEIN_ID = new QName(SERVICE_NS, "GlideinId");
+	public static final String GLIDEIN_FACTORY_SERVICE = "glidein/GlideinFactoryService";
+	public static final String GLIDEIN_SERVICE = "glidein/GlideinService";
 }
--- src/edu/usc/glidein/service/impl/SiteNames.java
+++ src/edu/usc/glidein/service/impl/SiteNames.java
@@ -2,11 +2,13 @@ package edu.usc.glidein.service.impl;
 
 import javax.xml.namespace.QName;
 
-public class SiteQNames
+public class SiteNames
 {
 	public static final String SERVICE_NS = "http://www.usc.edu/glidein/SiteService";
 	public static final QName RESOURCE_KEY = new QName(SERVICE_NS, "SiteServiceKey");
 	public static final QName RESOURCE_PROPERTIES = new QName(SERVICE_NS, "SiteResourceProperties");
 	
 	public static final QName RP_SITE_ID = new QName(SERVICE_NS, "SiteId");
+	public static final String SITE_FACTORY_SERVICE = "glidein/SiteFactoryService";
+	public static final String SITE_SERVICE = "glidein/SiteService";
 }
