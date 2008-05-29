2de97692864d8dc61b99540760b45b9e34dd7bbb
Gideon Juve <juve@usc.edu>
2008-05-29 15:35:56 +0000

    Remove glidein_environment table Remove getStatus Add newGlidein Add new fields (lastUpdate, submitted) Add site name to select queries
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1244 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/service/db/mysql/MySQLGlideinDAO.java
+++ src/edu/usc/glidein/service/db/mysql/MySQLGlideinDAO.java
@@ -4,15 +4,16 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Calendar;
 import java.util.LinkedList;
+import java.util.TimeZone;
 
 import edu.usc.glidein.service.db.DatabaseException;
 import edu.usc.glidein.service.db.GlideinDAO;
 import edu.usc.glidein.service.db.JDBCUtil;
-import edu.usc.glidein.stubs.types.EnvironmentVariable;
 import edu.usc.glidein.stubs.types.Glidein;
 import edu.usc.glidein.stubs.types.GlideinStatus;
-import edu.usc.glidein.stubs.types.GlideinStatusCode;
 
 public class MySQLGlideinDAO implements GlideinDAO
 {
@@ -30,7 +31,6 @@ public class MySQLGlideinDAO implements GlideinDAO
 		try {
 			conn = database.getConnection();
 			id = createGlidein(conn, glidein);
-			createEnvironment(conn, id, glidein.getEnvironment());
 			conn.commit();
 		} catch (DatabaseException dbe) {
 			JDBCUtil.rollbackQuietly(conn);
@@ -50,19 +50,20 @@ public class MySQLGlideinDAO implements GlideinDAO
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		try {
-			stmt = connection.prepareStatement("INSERT INTO glidein (site, count, hostCount, wallTime, numCpus, configBase64, gcbBroker, idleTime, debug, status, statusMessage) VALUES (?,?,?,?,?,?,?,?,?,?,?)");
+			stmt = connection.prepareStatement("INSERT INTO glidein (site, count, hostCount, wallTime, numCpus, condorConfigBase64, gcbBroker, idleTime, condorDebug, status, statusMessage, submitted, lastUpdate, condorHost) VALUES (?,?,?,?,?,?,?,?,?,?,?,NOW(),NOW(),?)");
 			int i = 1;
 			stmt.setInt(i++, glidein.getSiteId());
 			stmt.setInt(i++, glidein.getCount());
 			stmt.setInt(i++, glidein.getHostCount());
 			stmt.setInt(i++, glidein.getWallTime());
 			stmt.setInt(i++, glidein.getNumCpus());
-			stmt.setString(i++, glidein.getConfigBase64());
+			stmt.setString(i++, glidein.getCondorConfigBase64());
 			stmt.setString(i++, glidein.getGcbBroker());
 			stmt.setInt(i++, glidein.getIdleTime());
-			stmt.setString(i++, glidein.getDebug());
-			stmt.setString(i++, GlideinStatusCode.NEW.toString());
-			stmt.setString(i++, GlideinStatusCode.NEW.toString());
+			stmt.setString(i++, glidein.getCondorDebug());
+			stmt.setString(i++, glidein.getStatus().toString());
+			stmt.setString(i++, glidein.getStatusMessage());
+			stmt.setString(i++, glidein.getCondorHost());
 			if (stmt.executeUpdate()!=1) {
 				throw new DatabaseException("Unable to create glidein: wrong number of db updates");
 			}
@@ -82,44 +83,14 @@ public class MySQLGlideinDAO implements GlideinDAO
 			JDBCUtil.closeQuietly(stmt);
 		}
 		return id;
-	}
-
-	private void createEnvironment(Connection connection, int glideinId, EnvironmentVariable[] env)
-	throws DatabaseException
-	{
-		if (env == null || env.length == 0) return;
-		PreparedStatement stmt = null;
-		try {
-			stmt = connection.prepareStatement("INSERT INTO glidein_environment (glidein, variable, value) VALUES (?,?,?)");
-			for (EnvironmentVariable var : env) {
-				stmt.setInt(1, glideinId);
-				stmt.setString(2, var.getVariable());
-				stmt.setString(3, var.getValue());
-				stmt.addBatch();
-			}
-			int[] updates = stmt.executeBatch();
-			for (int update : updates) {
-				if (update!=1) {
-					throw new DatabaseException("Unable to create glidein environment: wrong number of db updates");
-				}
-			}
-		} catch(SQLException sqle) {
-			throw new DatabaseException("Unable to create glidein environment: insert failed",sqle);
-		} finally {
-			JDBCUtil.closeQuietly(stmt);
-		}
-	}
-		
+	}		
 
 	public Glidein load(int glideinId) throws DatabaseException
 	{
 		Connection conn = null;
 		try {
 			conn = database.getConnection();
-			Glidein glidein = getGlidein(conn, glideinId);
-			EnvironmentVariable[] env = getEnvironment(conn, glideinId);
-			glidein.setEnvironment(env);
-			return glidein;
+			return getGlidein(conn, glideinId);
 		} finally {
 			JDBCUtil.closeQuietly(conn);
 		}
@@ -130,22 +101,11 @@ public class MySQLGlideinDAO implements GlideinDAO
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		try {
-			stmt = connection.prepareStatement("SELECT * FROM glidein WHERE id=?");
+			stmt = connection.prepareStatement("SELECT g.*, s.name siteName FROM glidein g, site s WHERE g.id=? AND g.site=s.id");
 			stmt.setInt(1, glideinId);
 			rs = stmt.executeQuery();
 			if (rs.next()) {
-				Glidein glidein = new Glidein();
-				glidein.setId(glideinId);
-				glidein.setSiteId(rs.getInt("site"));
-				glidein.setCount(rs.getInt("count"));
-				glidein.setHostCount(rs.getInt("hostCount"));
-				glidein.setWallTime(rs.getInt("wallTime"));
-				glidein.setNumCpus(rs.getInt("numCpus"));
-				glidein.setConfigBase64(rs.getString("configBase64"));
-				glidein.setGcbBroker(rs.getString("gcbBroker"));
-				glidein.setIdleTime(rs.getInt("idleTime"));
-				glidein.setDebug(rs.getString("debug"));
-				return glidein;
+				return newGlidein(rs);
 			} else {
 				throw new DatabaseException("Glidein "+glideinId+" not found");
 			}
@@ -157,82 +117,9 @@ public class MySQLGlideinDAO implements GlideinDAO
 		}
 	}
 	
-	private EnvironmentVariable[] getEnvironment(Connection connection, int glideinId) throws DatabaseException
-	{
-		LinkedList<EnvironmentVariable> env = new LinkedList<EnvironmentVariable>();
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		try {
-			stmt = connection.prepareStatement("SELECT * FROM glidein_environment WHERE glidein=?");
-			stmt.setInt(1, glideinId);
-			rs = stmt.executeQuery();
-			if (rs.next()) {
-				String variable = rs.getString("variable");
-				String value = rs.getString("value");
-				env.add(new EnvironmentVariable(value,variable));
-			}
-		} catch(SQLException sqle) {
-			throw new DatabaseException("Unable to load glidein environment: select failed",sqle);
-		} finally {
-			JDBCUtil.closeQuietly(rs);
-			JDBCUtil.closeQuietly(stmt);
-		}
-		return (EnvironmentVariable[])env.toArray(new EnvironmentVariable[0]);
-	}
-	
 	public void store(Glidein glidein) throws DatabaseException
 	{
-		Connection conn = null;
-		int glideinId = glidein.getId();
-		try {
-			conn = database.getConnection();
-			updateGlidein(conn, glidein);
-			updateEnvironment(conn, glideinId, glidein.getEnvironment());
-			conn.commit();
-		} catch (DatabaseException dbe) {
-			JDBCUtil.rollbackQuietly(conn);
-			throw dbe;
-		} catch (SQLException sqle) {
-			JDBCUtil.rollbackQuietly(conn);
-			throw new DatabaseException("Unable to store site: commit failed",sqle);
-		} finally {
-			JDBCUtil.closeQuietly(conn);
-		}
-	}
-
-	private void updateGlidein(Connection connection, Glidein glidein) throws DatabaseException
-	{
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		try {
-			stmt = connection.prepareStatement("UPDATE glidein SET site=?, count=?, hostCount=?, wallTime=?, numCpus=?, configBase64=?, gcbBroker=?, idleTime=?, debug=? WHERE id=?");
-			int i = 1;
-			stmt.setInt(i++, glidein.getSiteId());
-			stmt.setInt(i++, glidein.getCount());
-			stmt.setInt(i++, glidein.getHostCount());
-			stmt.setInt(i++, glidein.getWallTime());
-			stmt.setInt(i++, glidein.getNumCpus());
-			stmt.setString(i++, glidein.getConfigBase64());
-			stmt.setString(i++, glidein.getGcbBroker());
-			stmt.setInt(i++, glidein.getIdleTime());
-			stmt.setString(i++, glidein.getDebug());
-			stmt.setInt(i++, glidein.getId());
-			if (stmt.executeUpdate()!=1) {
-				throw new DatabaseException("Unable to store site: wrong number of db updates");
-			}
-		} catch (SQLException sqle) {
-			throw new DatabaseException("Unable to store site: update failed",sqle);
-		} finally {
-			JDBCUtil.closeQuietly(rs);
-			JDBCUtil.closeQuietly(stmt);	
-		}
-	}
-	
-	private void updateEnvironment(Connection connection, int glideinId, EnvironmentVariable[] env)
-	throws DatabaseException
-	{
-		deleteEnvironment(connection, glideinId);
-		createEnvironment(connection, glideinId, env);
+		updateStatus(glidein.getId(), glidein.getStatus(), glidein.getStatusMessage());
 	}
 	
 	public void delete(int glideinId) throws DatabaseException
@@ -260,55 +147,15 @@ public class MySQLGlideinDAO implements GlideinDAO
 		}
 	}
 	
-	private void deleteEnvironment(Connection connection, int glideinId) throws DatabaseException
-	{
-		PreparedStatement stmt = null;
-		try {
-			stmt = connection.prepareStatement("DELETE FROM glidein_environment WHERE glidein=?");
-			stmt.setInt(1, glideinId);
-			stmt.executeUpdate();
-		} catch(SQLException sqle) {
-			throw new DatabaseException("Unable to store glidein environment: delete failed",sqle);
-		} finally {
-			JDBCUtil.closeQuietly(stmt);
-		}
-	}
-	
-	public GlideinStatus getStatus(int glideinId) throws DatabaseException
-	{
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		try {
-			stmt = conn.prepareStatement("SELECT status, statusMessage FROM glidein WHERE id=?");
-			stmt.setInt(1, glideinId);
-			rs = stmt.executeQuery();
-			if (rs.next()) {
-				GlideinStatus status = new GlideinStatus();
-				status.setCode(GlideinStatusCode.fromString(rs.getString("status")));
-				status.setMessage(rs.getString("statusMessage"));
-				return status;
-			} else {
-				throw new DatabaseException("Unable to get glidein status: no record found");
-			}
-		} catch (SQLException sqle) {
-			throw new DatabaseException("Unable to get glidein status: select failed",sqle);
-		} finally {
-			JDBCUtil.closeQuietly(rs);
-			JDBCUtil.closeQuietly(stmt);
-			JDBCUtil.closeQuietly(conn);
-		}
-	}
-	
-	public void updateStatus(int glideinId, GlideinStatus status)
+	public void updateStatus(int glideinId, GlideinStatus status, String statusMessage)
 	throws DatabaseException
 	{
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		try {
 			stmt = conn.prepareStatement("UPDATE glidein SET status=?, statusMessage=? WHERE id=?");
-			stmt.setString(1, status.getCode().toString());
-			stmt.setString(2, status.getMessage());
+			stmt.setString(1, status.toString());
+			stmt.setString(2, statusMessage);
 			stmt.setInt(3, glideinId);
 			if (stmt.executeUpdate()!=1) {
 				throw new DatabaseException("Unable to update glidein status: wrong number of db updates");
@@ -325,5 +172,72 @@ public class MySQLGlideinDAO implements GlideinDAO
 			JDBCUtil.closeQuietly(conn);
 		}
 		
+	}
+	
+	public Glidein[] list(boolean longFormat) throws DatabaseException 
+	{
+		Connection conn = null;
+		try {
+			conn = database.getConnection();
+			return getGlideins(conn, longFormat);
+		} finally {
+			JDBCUtil.closeQuietly(conn);
+		}
+	}
+	
+	private Glidein[] getGlideins(Connection connection, boolean longFormat) throws DatabaseException
+	{
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		LinkedList<Glidein> results = new LinkedList<Glidein>();
+		try {
+			// Join to get the site name
+			stmt = connection.prepareStatement("SELECT g.*, s.name siteName FROM glidein g, site s WHERE g.site=s.id");
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+				results.add(newGlidein(rs));
+			}
+			return results.toArray(new Glidein[0]);
+		} catch(SQLException sqle) {
+			throw new DatabaseException("Unable to load glidein: select failed",sqle);
+		} finally {
+			JDBCUtil.closeQuietly(rs);
+			JDBCUtil.closeQuietly(stmt);
+		}
+	}
+	
+	private Glidein newGlidein(ResultSet rs) throws DatabaseException
+	{
+		try {
+			Glidein glidein = new Glidein();
+			glidein.setId(rs.getInt("id"));
+			glidein.setSiteId(rs.getInt("site"));
+			glidein.setSiteName(rs.getString("siteName"));
+			glidein.setCount(rs.getInt("count"));
+			glidein.setHostCount(rs.getInt("hostCount"));
+			glidein.setWallTime(rs.getInt("wallTime"));
+			glidein.setNumCpus(rs.getInt("numCpus"));
+			glidein.setCondorConfigBase64(rs.getString("condorConfigBase64"));
+			glidein.setGcbBroker(rs.getString("gcbBroker"));
+			glidein.setIdleTime(rs.getInt("idleTime"));
+			glidein.setCondorDebug(rs.getString("condorDebug"));
+			glidein.setCondorHost(rs.getString("condorHost"));
+			
+			glidein.setStatus(GlideinStatus.fromString(rs.getString("status")));
+			glidein.setStatusMessage(rs.getString("statusMessage"));
+			
+			Calendar submitted = Calendar.getInstance(TimeZone.getDefault());
+			Timestamp submit = rs.getTimestamp("submitted",submitted);
+			submitted.setTime(submit);
+			glidein.setSubmitted(submitted);
+			
+			Calendar lastUpdate = Calendar.getInstance(TimeZone.getDefault());
+			Timestamp last = rs.getTimestamp("lastUpdate",lastUpdate);
+			lastUpdate.setTime(last);
+			glidein.setLastUpdate(lastUpdate);
+			return glidein;
+		} catch (SQLException sqle) {
+			throw new DatabaseException("Unable to create Glidein object",sqle);
+		}
 	}
 }
