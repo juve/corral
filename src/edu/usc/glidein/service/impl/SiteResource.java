cccdbaddd46f0daaf5abb496cef3b4eb22e809dc
Gideon Juve <juve@usc.edu>
2008-06-02 23:04:21 +0000

    Add submit Add updateStatus
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1265 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/service/impl/SiteResource.java
+++ src/edu/usc/glidein/service/impl/SiteResource.java
@@ -1,5 +1,7 @@
 package edu.usc.glidein.service.impl;
 
+import org.apache.log4j.Logger;
+import org.globus.gsi.GlobusCredential;
 import org.globus.wsrf.ResourceException;
 import org.globus.wsrf.ResourceKey;
 import org.globus.wsrf.PersistentResource;
@@ -8,20 +10,27 @@ import org.globus.wsrf.ResourcePropertySet;
 import org.globus.wsrf.impl.ReflectionResourceProperty;
 import org.globus.wsrf.impl.SimpleResourcePropertySet;
 
+import edu.usc.glidein.GlideinException;
+import edu.usc.glidein.service.StageSiteOperation;
 import edu.usc.glidein.service.db.Database;
 import edu.usc.glidein.service.db.DatabaseException;
 import edu.usc.glidein.service.db.SiteDAO;
 import edu.usc.glidein.stubs.types.Site;
+import edu.usc.glidein.stubs.types.SiteStatus;
+import edu.usc.glidein.util.ProxyUtil;
 
 public class SiteResource implements PersistentResource, ResourceProperties
 {
+	private Logger logger = Logger.getLogger(SiteResource.class);
 	private SimpleResourcePropertySet resourceProperties;
 	private Site site;
 	
 	/**
 	 * Default constructor required
 	 */
-	public SiteResource() { }
+	public SiteResource() {
+		
+	}
 	
 	public SiteResource(Site site) throws ResourceException
 	{
@@ -29,12 +38,25 @@ public class SiteResource implements PersistentResource, ResourceProperties
 		setResourceProperties();
 	}
 	
-	public void setSite(Site site) {
-		this.site = site;
+	public Site getSite()
+	{
+		return site;
 	}
 	
-	public Site getSite() {
-		return site;
+	/** 
+	 * @see org.globus.wsrf.ResourceIdentifier 
+	 */
+	public Object getID()
+	{
+		return new Integer(site.getId());
+	}
+	
+	/**
+	 * @see org.globus.wsrf.ResourceProperties
+	 */
+	public ResourcePropertySet getResourcePropertySet()
+	{
+		return resourceProperties;
 	}
 	
 	private void setResourceProperties() throws ResourceException
@@ -43,14 +65,18 @@ public class SiteResource implements PersistentResource, ResourceProperties
 			resourceProperties = new SimpleResourcePropertySet(SiteNames.RESOURCE_PROPERTIES);
 			resourceProperties.add(new ReflectionResourceProperty(
 					SiteNames.RP_SITE_ID,"Id",site));
-			// TODO Set the rest of the resource properties
+			// TODO: Set the rest of the resource properties, or don't, they seem kinda useless to me
 		} catch(Exception e) {
 			throw new ResourceException("Unable to set site resource properties",e);
 		}
 	}
 	
+	/**
+	 * Create the resource
+	 */
 	public synchronized void create() throws ResourceException
 	{
+		logger.debug("Creating site resource "+getSite().getId());
 		try {
 			Database db = Database.getDatabase();
 			SiteDAO dao = db.getSiteDAO();
@@ -60,9 +86,13 @@ public class SiteResource implements PersistentResource, ResourceProperties
 		}
 	}
 
+	/**
+	 * Load the resource
+	 * @see org.globus.wsrf.PersistenceCallback
+	 */
 	public synchronized void load(ResourceKey key) throws ResourceException
 	{
-		System.out.println("Loading "+key.getValue()); //XXX
+		logger.debug("Loading site resource "+key.getValue());
 		int id = ((Integer)key.getValue()).intValue();
 		try {
 			Database db = Database.getDatabase();
@@ -74,9 +104,13 @@ public class SiteResource implements PersistentResource, ResourceProperties
 		}
 	}
 	
+	/**
+	 * Store the resource
+	 * @see org.globus.wsrf.PersistenceCallback
+	 */
 	public synchronized void store() throws ResourceException
 	{
-		System.out.println("Storing "+getSite().getId()); //XXX
+		logger.debug("Storing site resource "+getSite().getId());
 		try {
 			Database db = Database.getDatabase();
 			SiteDAO dao = db.getSiteDAO();
@@ -85,9 +119,36 @@ public class SiteResource implements PersistentResource, ResourceProperties
 			throw new ResourceException(de);
 		}
 	}
+
+	/**
+	 * Remove the resource
+	 * 
+	 * NOTE: Don't get confused, this method is called when the ResourceHome
+	 * decides to remove this resource from its cache, not when the client
+	 * invokes the remove operation. The delete method is called when the
+	 * client invokes the remove operation.
+	 * 
+	 * TODO: Need to work out the issues around remove. The home should not
+	 * be able to remove anything that is running, or else the service will
+	 * get confused because the service will not see the updates caused
+	 * by the thread watching the job.
+	 * 
+	 * @see org.globus.wsrf.RemoveCallback
+	 */
+	public synchronized void remove() throws ResourceException
+	{
+		logger.debug("Removing site resource "+getSite().getId());
+	}
 	
+	/**
+	 * Delete the resource from the database
+	 * @throws ResourceException
+	 */
 	public synchronized void delete() throws ResourceException
 	{
+		// TODO: Check status and cancel staging job if necessary
+		
+		// Remove the site from the database
 		try {
 			Database db = Database.getDatabase();
 			SiteDAO dao = db.getSiteDAO();
@@ -99,15 +160,40 @@ public class SiteResource implements PersistentResource, ResourceProperties
 		}
 	}
 	
-	public Object getID() {
-		return new Integer(site.getId());
-	}
-
-	public void remove() throws ResourceException {
+	/**
+	 * Submit a staging job to the remote site
+	 */
+	public synchronized void submit() throws ResourceException 
+	{
+		logger.debug("Submitting site "+getSite().getId());
+		try {
+			// Get delegated credential
+			GlobusCredential cred = ProxyUtil.getCallerCredential();
 		
+			// Submit the staging job
+			StageSiteOperation operation = new StageSiteOperation(this, cred);
+			operation.invoke();
+		} catch (GlideinException ge) {
+			throw new ResourceException("Unable to submit site: "+ge.getMessage(),ge);
+		}
 	}
-
-	public ResourcePropertySet getResourcePropertySet() {
-		return resourceProperties;
+	
+	public synchronized void updateStatus(SiteStatus status, String statusMessage)
+	throws ResourceException
+	{
+		logger.debug("Changing status of site "+getSite().getId()+" to "+status+": "+statusMessage);
+		
+		// Update object
+		site.setStatus(status);
+		site.setStatusMessage(statusMessage);
+		
+		// Update database
+		try {
+			Database db = Database.getDatabase();
+			SiteDAO dao = db.getSiteDAO();
+			dao.updateStatus(site.getId(), status, statusMessage);
+		} catch(DatabaseException de) {
+			throw new ResourceException(de);
+		}
 	}
 }
