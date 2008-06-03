4b9e3ecb1da4ae2ade3b63114983efedc62e29a9
Gideon Juve <juve@usc.edu>
2008-06-03 18:28:27 +0000

    Move away from caching ResourceHomeImpl, pull stuff direct from the database
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1271 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/service/impl/GlideinResourceHome.java
+++ src/edu/usc/glidein/service/impl/GlideinResourceHome.java
@@ -1,25 +1,42 @@
 package edu.usc.glidein.service.impl;
 
+import javax.xml.namespace.QName;
+
 import org.apache.log4j.Logger;
+import org.globus.wsrf.InvalidResourceKeyException;
+import org.globus.wsrf.NoSuchResourceException;
+import org.globus.wsrf.RemoveNotSupportedException;
+import org.globus.wsrf.Resource;
 import org.globus.wsrf.ResourceException;
 import org.globus.wsrf.ResourceKey;
-import org.globus.wsrf.impl.ResourceHomeImpl;
+import org.globus.wsrf.ResourceHome;
 import org.globus.wsrf.impl.SimpleResourceKey;
+import org.globus.wsrf.jndi.Initializable;
 
 import edu.usc.glidein.service.db.Database;
 import edu.usc.glidein.service.db.DatabaseException;
 import edu.usc.glidein.service.db.GlideinDAO;
 import edu.usc.glidein.stubs.types.Glidein;
 
-public class GlideinResourceHome extends ResourceHomeImpl
+public class GlideinResourceHome implements ResourceHome, Initializable
 {
 	private Logger logger = Logger.getLogger(GlideinResourceHome.class);
 	
 	public synchronized void initialize() throws Exception
 	{
 		logger.info("Initializing glideins...");
-		super.initialize();
+		
 		// TODO: Recover glidein state
+	}
+	
+	public Class getKeyTypeClass()
+	{
+		return Integer.class;
+	}
+	
+	public QName getKeyTypeName()
+	{
+		return GlideinNames.RESOURCE_KEY;
 	}
 	
 	public ResourceKey create(Glidein glidein)
@@ -27,13 +44,32 @@ public class GlideinResourceHome extends ResourceHomeImpl
 	{
 		GlideinResource resource = new GlideinResource(glidein);
 		resource.create();
-		ResourceKey key = createKey(glidein.getId());
-		this.add(key, resource);
+		ResourceKey key = new SimpleResourceKey(
+				getKeyTypeName(), new Integer(glidein.getId()));
 		return key;
 	}
 	
-	public ResourceKey createKey(int id) {
-		return new SimpleResourceKey(getKeyTypeName(), new Integer(id));
+	public Resource find(ResourceKey key)
+	throws ResourceException, NoSuchResourceException, 
+		   InvalidResourceKeyException
+	{
+		if (key == null) {
+			throw new InvalidResourceKeyException();
+		}
+		GlideinResource r = new GlideinResource();
+		r.load(key);
+		return r;
+	}
+	
+	public void remove(ResourceKey key) 
+	throws ResourceException, NoSuchResourceException,
+		   InvalidResourceKeyException, RemoveNotSupportedException
+	{
+		if (key == null) {
+			throw new InvalidResourceKeyException();
+		}
+		GlideinResource r = (GlideinResource)find(key);
+		r.remove();
 	}
 
 	public Glidein[] list(boolean longFormat)
--- src/edu/usc/glidein/service/impl/SiteResourceHome.java
+++ src/edu/usc/glidein/service/impl/SiteResourceHome.java
@@ -1,30 +1,44 @@
 package edu.usc.glidein.service.impl;
 
+import javax.xml.namespace.QName;
+
 import org.apache.log4j.Logger;
+import org.globus.wsrf.InvalidResourceKeyException;
+import org.globus.wsrf.NoSuchResourceException;
+import org.globus.wsrf.RemoveNotSupportedException;
+import org.globus.wsrf.Resource;
 import org.globus.wsrf.ResourceException;
+import org.globus.wsrf.ResourceHome;
 import org.globus.wsrf.ResourceKey;
-import org.globus.wsrf.impl.ResourceHomeImpl;
 import org.globus.wsrf.impl.SimpleResourceKey;
+import org.globus.wsrf.jndi.Initializable;
 
 import edu.usc.glidein.service.db.Database;
 import edu.usc.glidein.service.db.DatabaseException;
 import edu.usc.glidein.service.db.SiteDAO;
 import edu.usc.glidein.stubs.types.Site;
 
-public class SiteResourceHome extends ResourceHomeImpl
+public class SiteResourceHome implements ResourceHome, Initializable
 {
 	private Logger logger = Logger.getLogger(SiteResourceHome.class);
 	
 	public synchronized void initialize() throws Exception
 	{
 		logger.info("Initializing sites...");
-		super.initialize();
+		
 		// TODO: Recover site state
 	}
 	
-	/**
-	 * Create a new site and add it
-	 */
+	public Class getKeyTypeClass()
+	{
+		return Integer.class;
+	}
+	
+	public QName getKeyTypeName() 
+	{
+		return SiteNames.RESOURCE_KEY;
+	}
+	
 	public ResourceKey create(Site site)
 	throws ResourceException
 	{
@@ -32,13 +46,32 @@ public class SiteResourceHome extends ResourceHomeImpl
 		resource.create();
 		ResourceKey key = new SimpleResourceKey(
 				getKeyTypeName(), new Integer(site.getId()));
-		this.add(key, resource);
 		return key;
 	}
 
-	/**
-	 * List all the sites in the database
-	 */
+	public Resource find(ResourceKey key) 
+	throws ResourceException, NoSuchResourceException, 
+		   InvalidResourceKeyException
+	{
+		if (key == null) {
+			throw new InvalidResourceKeyException();
+		}
+		SiteResource r = new SiteResource();
+		r.load(key);
+		return r;
+	}
+	
+	public void remove(ResourceKey key) 
+	throws ResourceException, NoSuchResourceException, 
+		   InvalidResourceKeyException, RemoveNotSupportedException 
+	{
+		if (key == null) {
+			throw new InvalidResourceKeyException();
+		}
+		SiteResource r = (SiteResource)find(key);
+		r.remove();
+	}
+
 	public Site[] list(boolean longFormat) 
 	throws ResourceException
 	{
