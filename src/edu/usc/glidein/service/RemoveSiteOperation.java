f919e3cbbbcb8e167d7fcd8c0ab6daee8979f037
Gideon Juve <juve@usc.edu>
2008-06-04 01:18:43 +0000

    Start implementing RemoveSiteOperation
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1283 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/service/RemoveSiteOperation.java
+++ src/edu/usc/glidein/service/RemoveSiteOperation.java
@@ -1,9 +1,44 @@
 package edu.usc.glidein.service;
 
+import org.globus.gsi.GlobusCredential;
+import org.globus.wsrf.ResourceKey;
+
+import edu.usc.glidein.service.impl.SiteResource;
+import edu.usc.glidein.service.impl.SiteResourceHome;
+import edu.usc.glidein.util.AddressingUtil;
+import edu.usc.glidein.util.NamingUtil;
+
 public class RemoveSiteOperation
 {
-	public RemoveSiteOperation()
+	private SiteResource resource = null;
+	private GlobusCredential credential = null;
+	
+	public RemoveSiteOperation(SiteResource res, GlobusCredential cred)
+	{
+		this.resource = res;
+		this.credential = cred;
+	}
+	
+	public void invoke()
 	{
 		
+	}
+	
+	public void remove() throws Exception
+	{
+		// TODO: Cancel staging operations
+		
+		// TODO: Cancel running glideins
+		
+		// TODO: Submit uninstall job
+		
+		ResourceKey key = AddressingUtil.getSiteKey(resource.getSite().getId());
+		
+		// Delete the site
+		resource.delete();
+		
+		// Remove the site from the resource home
+		SiteResourceHome resourceHome = NamingUtil.getSiteResourceHome();
+		resourceHome.remove(key);
 	}
 }
