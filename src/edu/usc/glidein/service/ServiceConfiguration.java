459375e878355c9446e9f00cc10ed702e8bbf7dc
Gideon Juve <juve@usc.edu>
2008-06-10 01:56:12 +0000

    Use absolute paths everywhere Match getters and setters for all config properties
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1304 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/service/ServiceConfiguration.java
+++ src/edu/usc/glidein/service/ServiceConfiguration.java
@@ -1,90 +1,107 @@
 package edu.usc.glidein.service;
 
+import java.io.File;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
-
-import edu.usc.glidein.GlideinException;
+import javax.naming.NamingException;
 
 public class ServiceConfiguration
 {
-	private String install;
-	private String uninstall;
-	private String run;
+	private File install;
+	private File uninstall;
+	private File run;
 	private String[] stagingURLs;
-	private String glideinCondorConfig;
-	private String tempDir;
+	private File glideinCondorConfig;
+	private File tempDir;
 	
-	public ServiceConfiguration() { }
+	public ServiceConfiguration()
+	{
+		this.tempDir = new File("/tmp");
+	}
 	
-	public static ServiceConfiguration getInstance()
-	throws GlideinException
+	public static ServiceConfiguration getInstance() throws NamingException
 	{
 		String location = "java:comp/env/glidein/ServiceConfiguration";
-		try {
-			Context initialContext = new InitialContext();
-	    	return (ServiceConfiguration)initialContext.lookup(location);
-		} catch (Exception e) {
-			throw new GlideinException("Unable to load configuration: "+location,e);
-		}
+		Context initialContext = new InitialContext();
+	    return (ServiceConfiguration)initialContext.lookup(location);
 	}
 
 	public String getInstall()
 	{
-		return install;
+		return install.getAbsolutePath();
 	}
 
 	public void setInstall(String install)
 	{
-		this.install = install;
+		this.install = new File(install);
 	}
 
 	public String getUninstall()
 	{
-		return uninstall;
+		return uninstall.getAbsolutePath();
 	}
 
 	public void setUninstall(String uninstall)
 	{
-		this.uninstall = uninstall;
+		this.uninstall = new File(uninstall);
 	}
 
 	public String getRun()
 	{
-		return run;
+		return run.getAbsolutePath();
 	}
 
 	public void setRun(String run)
 	{
-		this.run = run;
+		this.run = new File(run);
 	}
 
 	public String[] getStagingURLs()
 	{
 		return stagingURLs;
 	}
-
-	public void setStagingURLs(String stagingURLs)
+	
+	public void setStagingURLs(String[] stagingURLs)
 	{
-		this.stagingURLs = stagingURLs.split("[ ,;\t\n]+");
+		this.stagingURLs = stagingURLs;
+	}
+	
+	public String getGlideinServerURLs()
+	{
+		if (stagingURLs==null) {
+			return null;
+		}
+		StringBuffer buff = new StringBuffer();
+		for (int i=0; i<stagingURLs.length; i++) {
+			buff.append(stagingURLs[i]);
+			if (i<stagingURLs.length-1) buff.append(",");
+		}
+		return buff.toString();
+	}
+
+	public void setGlideinServerURLs(String glideinServerURLs)
+	{
+		this.stagingURLs = glideinServerURLs.split("[ ,;\t\n]+");
 	}
 
 	public String getGlideinCondorConfig()
 	{
-		return glideinCondorConfig;
+		return glideinCondorConfig.getAbsolutePath();
 	}
 
 	public void setGlideinCondorConfig(String glideinCondorConfig)
 	{
-		this.glideinCondorConfig = glideinCondorConfig;
+		this.glideinCondorConfig = new File(glideinCondorConfig);
 	}
 
 	public String getTempDir()
 	{
-		return tempDir;
+		return tempDir.getAbsolutePath();
 	}
 
 	public void setTempDir(String tempDir)
 	{
-		this.tempDir = tempDir;
+		this.tempDir = new File(tempDir);
 	}
 }
