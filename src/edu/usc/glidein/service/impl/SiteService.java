4216fba91ba7d18f2a8b86fa0fdebced4427ea1f
Gideon Juve <juve@usc.edu>
2008-06-10 02:03:35 +0000

    Add force flag
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1312 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/service/impl/SiteService.java
+++ src/edu/usc/glidein/service/impl/SiteService.java
@@ -34,9 +34,9 @@ public class SiteService
 		return new EmptyObject();
 	}
 	
-	public EmptyObject remove(EmptyObject empty) throws RemoteException
+	public EmptyObject remove(boolean force) throws RemoteException
 	{
-		getResource().remove();
+		getResource().remove(force);
 		return new EmptyObject();
 	}
 }
