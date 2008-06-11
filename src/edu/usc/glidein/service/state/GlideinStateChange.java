a3176bbf4d5ed0e99f90813c0636d9374f49ea4a
Gideon Juve <juve@usc.edu>
2008-06-11 17:18:18 +0000

    Implement state change objects
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1319 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/service/state/GlideinStateChange.java
+++ src/edu/usc/glidein/service/state/GlideinStateChange.java
@@ -1,19 +1,38 @@
 package edu.usc.glidein.service.state;
 
+import javax.naming.NamingException;
+
+import org.apache.log4j.Logger;
+import org.globus.wsrf.ResourceException;
 import org.globus.wsrf.ResourceKey;
+
+import edu.usc.glidein.service.impl.GlideinResource;
+import edu.usc.glidein.service.impl.GlideinResourceHome;
+import edu.usc.glidein.stubs.types.GlideinState;
 
 public class GlideinStateChange implements StateChange
 {
+	private Logger logger;
 	private ResourceKey key;
+	private GlideinState newState;
 	
-	public GlideinStateChange(ResourceKey key)
+	public GlideinStateChange(ResourceKey key, GlideinState newState)
 	{
+		this.logger = Logger.getLogger(GlideinStateChange.class);
 		this.key = key;
+		this.newState = newState;
 	}
 
 	public void run()
 	{
-		// TODO: Implement run()
-
+		try {
+			GlideinResourceHome home = GlideinResourceHome.getInstance();
+			GlideinResource resource = (GlideinResource) home.find(key);
+			resource.processStateChange(newState);
+		} catch (NamingException ne) {
+			logger.error("Unable to get GlideinResourceHome",ne);
+		} catch (ResourceException re) {
+			logger.error("Unable to find resource: "+key,re);
+		}
 	}
 }
--- src/edu/usc/glidein/service/state/SiteStateChange.java
+++ src/edu/usc/glidein/service/state/SiteStateChange.java
@@ -1,20 +1,39 @@
 package edu.usc.glidein.service.state;
 
+import javax.naming.NamingException;
+
+import org.apache.log4j.Logger;
+import org.globus.wsrf.ResourceException;
 import org.globus.wsrf.ResourceKey;
+
+import edu.usc.glidein.service.impl.SiteResource;
+import edu.usc.glidein.service.impl.SiteResourceHome;
+import edu.usc.glidein.stubs.types.SiteState;
 
 public class SiteStateChange implements StateChange
 {
+	private Logger logger;
 	private ResourceKey key;
+	private SiteState newState;
 	
-	public SiteStateChange(ResourceKey key)
+	public SiteStateChange(ResourceKey key, SiteState newState)
 	{
+		this.logger = Logger.getLogger(SiteStateChange.class);
 		this.key = key;
+		this.newState = newState;
 	}
 
 	public void run()
 	{
-		// TODO: Implement run()
-		
+		try {
+			SiteResourceHome home = SiteResourceHome.getInstance();
+			SiteResource resource = (SiteResource)home.find(key);
+			resource.processStateChange(newState);
+		} catch (NamingException ne) {
+			logger.error("Unable to get SiteResourceHome",ne);
+		} catch (ResourceException re) {
+			logger.error("Unable to find SiteResource: "+key,re);
+		}
 	}
 
 }
