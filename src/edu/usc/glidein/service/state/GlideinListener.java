d3809d155a6b6146493054560ecc7c480d30e890
Gideon Juve <juve@usc.edu>
2008-06-13 23:33:13 +0000

    Remove testing code
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1342 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/service/state/GlideinListener.java
+++ src/edu/usc/glidein/service/state/GlideinListener.java
@@ -3,7 +3,6 @@ package edu.usc.glidein.service.state;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 import edu.usc.glidein.service.exec.Condor;
@@ -11,10 +10,6 @@ import edu.usc.glidein.service.exec.CondorEvent;
 import edu.usc.glidein.service.exec.CondorEventListener;
 import edu.usc.glidein.service.exec.CondorException;
 import edu.usc.glidein.service.exec.CondorJob;
-import edu.usc.glidein.stubs.types.ExecutionService;
-import edu.usc.glidein.stubs.types.Glidein;
-import edu.usc.glidein.stubs.types.Site;
-import edu.usc.glidein.stubs.types.ServiceType;
 import edu.usc.glidein.util.IOUtil;
 
 public class GlideinListener implements CondorEventListener
@@ -119,78 +114,6 @@ public class GlideinListener implements CondorEventListener
 				event.getGenerator().terminate();
 				glideinFailed("Glidein job aborted");
 				break;
-		}
-	}
-	
-	public static void main(String[] args)
-	{
-		// Configure logging
-		System.setProperty("log4j.defaultInitOverride", "true");
-		BasicConfigurator.configure();
-		
-//		String name = "dynamic";
-//		String installPath = "/home/geovault-00/juve/glidein";
-//		String localPath = "/home/geovault-00/juve/glidein/local";
-//		String fork = "dynamic.usc.edu:2119/jobmanager-fork";
-//		String pbs = "dynamic.usc.edu:2119/jobmanager-pbs";
-//		String queue = null;
-//		String project = null;
-//		String broker = "128.125.25.48";
-		
-		String name = "sdsc";
-		String installPath = "/users/gideon/glidein";
-		String localPath = "/gpfs/gideon/glidein/local";
-		String fork = "tg-login.sdsc.teragrid.org/jobmanager-fork";
-		String pbs = "tg-login.sdsc.teragrid.org/jobmanager-pbs";
-		String queue = "dque";
-		String project = "CSB246";
-		String broker = null;
-		
-//		String name = "mercury";
-//		String installPath = "/users/gideon/glidein";
-//		String localPath = "/gpfs/gideon/glidein/local";
-//		String fork = "grid-hg.ncsa.teragrid.org/jobmanager-fork";
-//		String pbs = "grid-hg.ncsa.teragrid.org/jobmanager-pbs";
-//		String queue = "normal";
-//		String project = "nqi";
-//		String broker = null;
-		
-		try 
-		{
-			ExecutionService stagingService = new ExecutionService();
-			stagingService.setServiceType(ServiceType.GT2);
-			stagingService.setServiceContact(fork);
-			
-			ExecutionService glideinService = new ExecutionService();
-			glideinService.setServiceType(ServiceType.GT2);
-			glideinService.setServiceContact(pbs);
-			glideinService.setQueue(queue);
-			glideinService.setProject(project);
-			
-			Site s = new Site();
-			s.setCondorVersion("7.0.0");
-			s.setName(name);
-			s.setInstallPath(installPath);
-			s.setLocalPath(localPath);
-			s.setStagingService(stagingService);
-			s.setGlideinService(glideinService);
-			
-			
-			Glidein g = new Glidein();
-			g.setCondorHost("array.usc.edu");
-			g.setWallTime(10); // 10 mins for test
-			g.setHostCount(1);
-			g.setNumCpus(2);
-			g.setGcbBroker(broker);
-			//gd.setIdleTime(1); // 1 min for test
-			//gd.setDebug("D_FULLDEBUG,D_DAEMONCORE");
-			
-			// TODO: Figure out how to test
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-			System.exit(1);
 		}
 	}
 }
--- src/edu/usc/glidein/service/state/StageSiteListener.java
+++ src/edu/usc/glidein/service/state/StageSiteListener.java
@@ -3,7 +3,6 @@ package edu.usc.glidein.service.state;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.globus.wsrf.ResourceKey;
 
@@ -12,9 +11,6 @@ import edu.usc.glidein.service.exec.CondorEvent;
 import edu.usc.glidein.service.exec.CondorEventListener;
 import edu.usc.glidein.service.exec.CondorException;
 import edu.usc.glidein.service.exec.CondorJob;
-import edu.usc.glidein.stubs.types.ExecutionService;
-import edu.usc.glidein.stubs.types.ServiceType;
-import edu.usc.glidein.stubs.types.Site;
 import edu.usc.glidein.util.IOUtil;
 
 public class StageSiteListener implements CondorEventListener
@@ -124,69 +120,6 @@ public class StageSiteListener implements CondorEventListener
 				event.getGenerator().terminate();
 				failed("Staging job aborted");
 				break;
-		}
-	}
-	
-	public static void main(String[] args)
-	{
-		// Configure logging
-		System.setProperty("log4j.defaultInitOverride", "true");
-		BasicConfigurator.configure();
-		
-		String name = "dynamic";
-		String installPath = "/home/geovault-00/juve/glidein";
-		String localPath = "/home/geovault-00/juve/glidein/local";
-		String fork = "dynamic.usc.edu:2119/jobmanager-fork";
-		String pbs = "dynamic.usc.edu:2119/jobmanager-pbs";
-		String queue = null;
-		String project = null;
-		String condorPackage = "7.0.0-x86-Linux-2.6-glibc2.3.tar.gz";
-		
-//		String name = "sdsc";
-//		String installPath = "/users/gideon/glidein";
-//		String localPath = "/gpfs/gideon/glidein/local";
-//		String fork = "tg-login.sdsc.teragrid.org/jobmanager-fork";
-//		String pbs = "tg-login.sdsc.teragrid.org/jobmanager-pbs";
-//		String queue = "dque";
-//		String project = "CSB246";
-//		String condorPackage = null;
-		
-//		String name = "mercury";
-//		String installPath = "/users/gideon/glidein";
-//		String localPath = "/gpfs/gideon/glidein/local";
-//		String fork = "grid-hg.ncsa.teragrid.org/jobmanager-fork";
-//		String pbs = "grid-hg.ncsa.teragrid.org/jobmanager-pbs";
-//		String queue = "normal";
-//		String project = "nqi";
-//		String condorPackage = null;
-		
-		try 
-		{
-			ExecutionService stagingService = new ExecutionService();
-			stagingService.setServiceType(ServiceType.GT2);
-			stagingService.setServiceContact(fork);
-			
-			ExecutionService glideinService = new ExecutionService();
-			glideinService.setServiceType(ServiceType.GT2);
-			glideinService.setServiceContact(pbs);
-			glideinService.setQueue(queue);
-			glideinService.setProject(project);
-			
-			Site s = new Site();
-			s.setCondorVersion("7.0.0");
-			s.setName(name);
-			s.setInstallPath(installPath);
-			s.setLocalPath(localPath);
-			s.setStagingService(stagingService);
-			s.setGlideinService(glideinService);
-			s.setCondorPackage(condorPackage);
-			
-			// TODO: Figure out how to test
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-			System.exit(1);
 		}
 	}
 }
