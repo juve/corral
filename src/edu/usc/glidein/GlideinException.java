b05908255df0e47850559323cdc8c04e42be6e37
Gideon Juve <juve@usc.edu>
2008-06-16 22:40:26 +0000

    Add project todo tasks
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1355 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/GlideinException.java
+++ src/edu/usc/glidein/GlideinException.java
@@ -1,5 +1,14 @@
 package edu.usc.glidein;
 
+/* Project todo tasks */
+// TODO: Validate certificate lifetime
+// TODO: Implement file persistence
+// TODO: Add resource properties?
+// TODO: Add history tracking
+// TODO: Test custom glidein_condor_config
+// TODO: Improve dir hierarchy on remote 
+// TODO: Retest GT4 @ SDSC
+
 public class GlideinException extends Exception
 {
 	private static final long serialVersionUID = 2767483133159443163L;
