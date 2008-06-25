92e82e09f5fb03042ca9d84b0b856ebb5450c96c
Gideon Juve <juve@usc.edu>
2008-06-25 00:30:45 +0000

    Implement auto-install of database tables
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1399 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/service/db/mysql/MySQLDatabase.java
+++ src/edu/usc/glidein/service/db/mysql/MySQLDatabase.java
@@ -1,23 +1,33 @@
 package edu.usc.glidein.service.db.mysql;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
+import org.apache.log4j.Logger;
 import org.globus.wsrf.jndi.Initializable;
 
 import edu.usc.glidein.service.db.DatabaseException;
 import edu.usc.glidein.service.db.GlideinDAO;
 import edu.usc.glidein.service.db.Database;
+import edu.usc.glidein.service.db.JDBCUtil;
 import edu.usc.glidein.service.db.SiteDAO;
+import edu.usc.glidein.util.IOUtil;
 
 public class MySQLDatabase extends Database implements Initializable
 {
+	public static final Logger logger = Logger.getLogger(MySQLDatabase.class);
 	public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
 	
 	private String url;
 	private String user;
 	private String password;
+	private String schemaPath;
+	private boolean autoInstall;
 	
 	public MySQLDatabase() { }
 	
@@ -33,7 +43,69 @@ public class MySQLDatabase extends Database implements Initializable
 		// Load database driver
 		Class.forName(DB_DRIVER).newInstance();
 		
-		// TODO: Install tables if they don't exist already
+		// If we are allowing auto installation of the database tables
+		if (isAutoInstall()) {
+			
+			// If tables aren't installed
+			if (!tablesInstalled()) {
+				logger.info("Installing database tables");
+				
+				// Run script
+				runDatabaseScript();
+				
+				// Check interface table again
+				if (!tablesInstalled()) {
+					throw new DatabaseException("Unable to install tables");
+				}
+			}
+		}
+	}
+	
+	private void runDatabaseScript() throws DatabaseException
+	{
+		File script = new File(getSchemaPath());
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			conn = getConnection();
+			stmt = conn.createStatement();
+			for (String s : getStatements(script)) {
+				stmt.addBatch(s);
+			}
+			stmt.executeBatch();
+		} catch (SQLException sqle) {
+			throw new DatabaseException("Unable to run database setup script");
+		} catch (IOException ioe) {
+			throw new DatabaseException("Unable to read database setup script");
+		} finally {
+			JDBCUtil.closeQuietly(stmt);
+			JDBCUtil.closeQuietly(conn);
+		}
+	}
+	
+	private boolean tablesInstalled() throws DatabaseException
+	{
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			conn = _getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery("SHOW TABLES LIKE 'interface'");
+			return rs.next();
+		} catch (SQLException sqle) {
+			throw new DatabaseException(
+					"Unable to determine if tables are installed");
+		} finally {
+			JDBCUtil.closeQuietly(stmt);
+			JDBCUtil.closeQuietly(conn);
+		}
+	}
+	
+	private String[] getStatements(File script) throws IOException
+	{
+		String stmts = IOUtil.read(script);
+		return stmts.split("[;]");
 	}
 	
 	public String getUrl()
@@ -66,6 +138,26 @@ public class MySQLDatabase extends Database implements Initializable
 		this.password = password;
 	}
 
+	public String getSchemaPath()
+	{
+		return schemaPath;
+	}
+
+	public void setSchemaPath(String schemaPath)
+	{
+		this.schemaPath = schemaPath;
+	}
+
+	public boolean isAutoInstall()
+	{
+		return autoInstall;
+	}
+
+	public void setAutoInstall(boolean autoInstall)
+	{
+		this.autoInstall = autoInstall;
+	}
+
 	public SiteDAO getSiteDAO()
 	{
 		return new MySQLSiteDAO(this);
@@ -76,13 +168,17 @@ public class MySQLDatabase extends Database implements Initializable
 		return new MySQLGlideinDAO(this);
 	}
 	
+	private Connection _getConnection() throws SQLException 
+	{
+		Connection conn = DriverManager.getConnection(url,user,password);
+		conn.setAutoCommit(false);
+		return conn;
+	}
+	
 	public Connection getConnection() throws DatabaseException
 	{
-		Connection connection;
 		try {
-			connection = DriverManager.getConnection(url,user,password);
-			connection.setAutoCommit(false);
-			return connection;
+			return _getConnection();
 		} catch(SQLException sqle) {
 			throw new DatabaseException("Unable to connect to database",sqle);
 		}
