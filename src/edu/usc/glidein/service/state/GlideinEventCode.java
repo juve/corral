6574093a2ad783a264270f0a0b5ce5c78e34cfb9
Gideon Juve <juve@usc.edu>
2008-06-25 23:30:37 +0000

    Add some event codes
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1408 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/service/state/GlideinEventCode.java
+++ src/edu/usc/glidein/service/state/GlideinEventCode.java
@@ -4,10 +4,12 @@ public enum GlideinEventCode implements EventCode
 {
 	SUBMIT,			/* Submit requested */
 	QUEUED,			/* Glidein queued remotely */
-	SITE_READY,		/* Site is ready for submission */
+	SITE_READY,		/* Site entered ready state */
 	JOB_SUCCESS,	/* Glidein job exited successfully */
 	JOB_FAILURE,	/* Glidein job failed */
+	JOB_ABORTED,	/* Glidein job was aborted */
 	RUNNING,		/* Running remotely */
 	REMOVE,			/* Remove requested */
-	DELETE			/* Delete requested */
+	DELETE,			/* Delete requested */ 
+	SITE_FAILED		/* Site entered failed state */
 }
