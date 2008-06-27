30353ac42be219ecf24aa3d428347c2609635bb9
Gideon Juve <juve@usc.edu>
2008-06-27 23:53:34 +0000

    Add utility class for printing Sites Add utility class for printing Glideins
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1435 e217846f-e12e-0410-a4e5-89ccaea66ff7

---
+++ src/edu/usc/glidein/util/GlideinUtil.java
@@ -0,0 +1,49 @@
+package edu.usc.glidein.util;
+
+import java.io.PrintStream;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import edu.usc.glidein.stubs.types.Glidein;
+
+public class GlideinUtil
+{
+	public static void print(Glidein glidein)
+	{
+		print(glidein,System.out);
+	}
+	
+	public static void print(Glidein glidein, PrintStream out)
+	{
+		if (glidein == null) {
+			out.println("null");
+			return;
+		}
+		
+		out.printf("id = %d\n",glidein.getId());
+		out.printf("siteName = %s\n", glidein.getSiteName());
+		out.printf("siteId = %d\n", glidein.getSiteId());
+		out.printf("condorHost = %s\n",glidein.getCondorHost());
+		out.printf("count = %d\n", glidein.getCount());
+		out.printf("hostCount = %d\n", glidein.getHostCount());
+		out.printf("numCpus = %d\n", glidein.getNumCpus());
+		out.printf("wallTime = %d\n", glidein.getWallTime());
+		out.printf("idleTime = %d\n", glidein.getIdleTime());
+		
+		Calendar submitted = glidein.getSubmitted();
+		submitted.setTimeZone(TimeZone.getDefault());
+		out.printf("submitted = %tc\n",submitted);
+		
+		Calendar lastUpdate = glidein.getLastUpdate();
+		lastUpdate.setTimeZone(TimeZone.getDefault());
+		out.printf("lastUpdate = %tc\n",lastUpdate);
+		
+		out.printf("state = %s\n",glidein.getState().toString());
+		out.printf("shortMessage = %s\n",glidein.getShortMessage());
+		out.printf("longMessage = %s\n",glidein.getLongMessage());
+		
+		out.printf("condorDebug = %s\n", glidein.getCondorDebug());
+		out.printf("gcbBroker = %s\n", glidein.getGcbBroker());
+		out.printf("resubmit = %s\n", glidein.isResubmit());
+	}
+}
---
+++ src/edu/usc/glidein/util/SiteUtil.java
@@ -0,0 +1,79 @@
+package edu.usc.glidein.util;
+
+import java.io.PrintStream;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import edu.usc.glidein.stubs.types.EnvironmentVariable;
+import edu.usc.glidein.stubs.types.ExecutionService;
+import edu.usc.glidein.stubs.types.Site;
+
+public class SiteUtil
+{
+	public static void print(Site site)
+	{
+		print(site,System.out);
+	}
+	
+	public static void print(Site site, PrintStream out)
+	{
+		if (site==null) {
+			out.println("null");
+			return;
+		}
+		
+		out.printf("id = %s\n", site.getId());
+		out.printf("name = %s\n",site.getName());
+		
+		Calendar submitted = (Calendar)site.getSubmitted().clone();
+		submitted.setTimeZone(TimeZone.getDefault());
+		out.printf("submitted = %tc\n", submitted);
+		
+		Calendar lastUpdate = (Calendar)site.getLastUpdate().clone();
+		lastUpdate.setTimeZone(TimeZone.getDefault());
+		out.printf("lastUpdate = %tc\n", lastUpdate);
+		
+		out.printf("state = %s\n", site.getState().toString());
+		out.printf("shortMessage = %s\n", site.getShortMessage());
+		out.printf("longMessage = %s\n", site.getLongMessage());
+		
+		out.printf("installPath = %s\n", site.getInstallPath());
+		out.printf("localPath = %s\n", site.getLocalPath());
+		out.printf("condorVersion = %s\n", site.getCondorVersion());
+		out.printf("condorPackage = %s\n", site.getCondorPackage());
+		
+		// Environment
+		EnvironmentVariable[] env = site.getEnvironment();
+		out.printf("environment =");
+		if (env != null) {
+			for (EnvironmentVariable var : env) {
+				out.printf(" %s=%s", var.getVariable(), var.getValue());
+			}
+		}
+		out.printf("\n");
+		
+		// Staging Service
+		ExecutionService stagingService = site.getStagingService();
+		if (stagingService == null) {
+			out.printf("stagingService = null\n");
+		} else {
+			out.printf("stagingService = %s %s\n",
+					stagingService.getServiceType(),
+					stagingService.getServiceContact());
+			out.printf("stagingService.project = %s\n", stagingService.getProject());
+			out.printf("stagingService.queue = %s\n", stagingService.getQueue());
+		}
+		
+		// Glidein Service
+		ExecutionService glideinService = site.getGlideinService();
+		if (glideinService == null) {
+			out.printf("glideinService = null\n");
+		} else {
+			out.printf("glideinService = %s %s\n",
+					glideinService.getServiceType(),
+					glideinService.getServiceContact());
+			out.printf("glideinService.project = %s\n", glideinService.getProject());
+			out.printf("glideinService.queue = %s\n", glideinService.getQueue());
+		}
+	}
+}
