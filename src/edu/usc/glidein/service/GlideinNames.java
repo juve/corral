6be9effd6554429635dbb1c1f21d47d7c91f2970
Gideon Juve <juve@usc.edu>
2008-07-02 19:00:13 +0000

    Add the rest of the resource properties
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1464 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/service/GlideinNames.java
+++ src/edu/usc/glidein/service/GlideinNames.java
@@ -19,11 +19,33 @@ import javax.xml.namespace.QName;
 
 public class GlideinNames
 {
+	// Service names
+	public static final String GLIDEIN_FACTORY_SERVICE = "glidein/GlideinFactoryService";
+	public static final String GLIDEIN_SERVICE = "glidein/GlideinService";
+	
+	// Resource properties
 	public static final String SERVICE_NS = "http://www.usc.edu/glidein/GlideinService";
 	public static final QName RESOURCE_KEY = new QName(SERVICE_NS, "GlideinServiceKey");
 	public static final QName RESOURCE_PROPERTIES = new QName(SERVICE_NS, "GlideinResourceProperties");
 	
-	public static final QName RP_GLIDEIN_ID = new QName(SERVICE_NS, "GlideinId");
-	public static final String GLIDEIN_FACTORY_SERVICE = "glidein/GlideinFactoryService";
-	public static final String GLIDEIN_SERVICE = "glidein/GlideinService";
+	public static final QName RP_ID = new QName(SERVICE_NS, "Id");
+	public static final QName RP_SITE = new QName(SERVICE_NS, "Site");
+	public static final QName RP_CONDOR_HOST = new QName(SERVICE_NS, "CondorHost");
+	public static final QName RP_COUNT = new QName(SERVICE_NS, "Count");
+	public static final QName RP_HOST_COUNT = new QName(SERVICE_NS, "HostCount");
+	public static final QName RP_WALL_TIME = new QName(SERVICE_NS, "WallTime");
+	public static final QName RP_NUM_CPUS = new QName(SERVICE_NS, "NumCpus");
+	public static final QName RP_CONDOR_CONFIG = new QName(SERVICE_NS, "CondorConfig");
+	public static final QName RP_GCB_BROKER = new QName(SERVICE_NS, "GcbBroker");
+	public static final QName RP_IDLE_TIME = new QName(SERVICE_NS, "IdleTime");
+	public static final QName RP_CONDOR_DEBUG = new QName(SERVICE_NS, "CondorDebug");
+	public static final QName RP_STATE = new QName(SERVICE_NS, "State");
+	public static final QName RP_SHORT_MESSAGE = new QName(SERVICE_NS, "ShortMessage");
+	public static final QName RP_LONG_MESSAGE = new QName(SERVICE_NS, "LongMessage");
+	public static final QName RP_CREATED = new QName(SERVICE_NS, "Created");
+	public static final QName RP_LAST_UPDATE = new QName(SERVICE_NS, "LastUpdate");
+	public static final QName RP_RESUBMIT = new QName(SERVICE_NS, "Resubmit");
+	public static final QName RP_RESUBMITS = new QName(SERVICE_NS, "Resubmits");
+	public static final QName RP_UNTIL = new QName(SERVICE_NS, "Until");
+	public static final QName RP_SUBMITS = new QName(SERVICE_NS, "Submits");
 }
--- src/edu/usc/glidein/service/SiteNames.java
+++ src/edu/usc/glidein/service/SiteNames.java
@@ -19,11 +19,32 @@ import javax.xml.namespace.QName;
 
 public class SiteNames
 {
+	// Service names
+	public static final String SITE_FACTORY_SERVICE = "glidein/SiteFactoryService";
+	public static final String SITE_SERVICE = "glidein/SiteService";
+	
+	// Resource properties
 	public static final String SERVICE_NS = "http://www.usc.edu/glidein/SiteService";
 	public static final QName RESOURCE_KEY = new QName(SERVICE_NS, "SiteServiceKey");
 	public static final QName RESOURCE_PROPERTIES = new QName(SERVICE_NS, "SiteResourceProperties");
 	
-	public static final QName RP_SITE_ID = new QName(SERVICE_NS, "SiteId");
-	public static final String SITE_FACTORY_SERVICE = "glidein/SiteFactoryService";
-	public static final String SITE_SERVICE = "glidein/SiteService";
+	public static final QName RP_ID = new QName(SERVICE_NS, "Id");
+	public static final QName RP_NAME = new QName(SERVICE_NS, "Name");
+	public static final QName RP_INSTALL_PATH = new QName(SERVICE_NS, "InstallPath");
+	public static final QName RP_LOCAL_PATH = new QName(SERVICE_NS, "LocalPath");
+	public static final QName RP_CONDOR_PACKAGE = new QName(SERVICE_NS, "CondorPackage");
+	public static final QName RP_CONDOR_VERSION = new QName(SERVICE_NS, "CondorVersion");
+	public static final QName RP_STATE = new QName(SERVICE_NS, "State");
+	public static final QName RP_SHORT_MESSAGE = new QName(SERVICE_NS, "ShortMessage");
+	public static final QName RP_LONG_MESSAGE = new QName(SERVICE_NS, "LongMessage");
+	public static final QName RP_CREATED = new QName(SERVICE_NS, "Created");
+	public static final QName RP_LAST_UPDATE = new QName(SERVICE_NS, "LastUpdate");
+	public static final QName RP_STAGING_SERVICE_CONTACT = new QName(SERVICE_NS, "StagingService/ServiceContact");
+	public static final QName RP_STAGING_SERVICE_TYPE = new QName(SERVICE_NS, "StagingService/ServiceType");
+	public static final QName RP_STAGING_PROJECT = new QName(SERVICE_NS, "StagingService/Project");
+	public static final QName RP_STAGING_QUEUE = new QName(SERVICE_NS, "StagingService/Queue");
+	public static final QName RP_GLIDEIN_SERVICE_CONTACT = new QName(SERVICE_NS, "GlideinService/ServiceContact");
+	public static final QName RP_GLIDEIN_SERVICE_TYPE = new QName(SERVICE_NS, "GlideinService/ServiceType");
+	public static final QName RP_GLIDEIN_PROJECT = new QName(SERVICE_NS, "GlideinService/Project");
+	public static final QName RP_GLIDEIN_QUEUE = new QName(SERVICE_NS, "GlideinService/Queue");
 }
