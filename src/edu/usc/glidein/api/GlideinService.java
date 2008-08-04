8ec70dbbd9522e02e9ba37f2340ca654d161b52c
Gideon Juve <juve@usc.edu>
2008-08-04 20:06:02 +0000

    Update API to allow notifications
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1527 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/api/GlideinService.java
+++ src/edu/usc/glidein/api/GlideinService.java
@@ -17,20 +17,41 @@ package edu.usc.glidein.api;
 
 import java.net.URL;
 import java.rmi.RemoteException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.xml.rpc.ServiceException;
 import javax.xml.rpc.Stub;
 
 import org.apache.axis.message.addressing.EndpointReferenceType;
+import org.globus.wsrf.NotificationConsumerManager;
+import org.globus.wsrf.NotifyCallback;
+import org.globus.wsrf.WSNConstants;
+import org.globus.wsrf.encoding.DeserializationException;
+import org.globus.wsrf.encoding.ObjectDeserializer;
+import org.oasis.wsn.NotificationProducer;
+import org.oasis.wsn.Subscribe;
+import org.oasis.wsn.TopicExpressionType;
+import org.oasis.wsn.WSBaseNotificationServiceAddressingLocator;
+import org.w3c.dom.Element;
 
+import edu.usc.glidein.service.GlideinNames;
 import edu.usc.glidein.stubs.GlideinPortType;
 import edu.usc.glidein.stubs.service.GlideinServiceAddressingLocator;
 import edu.usc.glidein.stubs.types.EmptyObject;
 import edu.usc.glidein.stubs.types.Glidein;
+import edu.usc.glidein.stubs.types.GlideinStateChange;
+import edu.usc.glidein.stubs.types.GlideinStateChangeMessage;
 import edu.usc.glidein.util.AddressingUtil;
 
 public class GlideinService extends BaseService
 {	
+	private NotificationConsumerManager consumer = null;
+	private EndpointReferenceType consumerEPR = null;
+	private GlideinHandler handler = null;
+	private Set<GlideinListener> listeners = new HashSet<GlideinListener>();
+	
 	public GlideinService(EndpointReferenceType epr)
 	{
 		super(epr);
@@ -84,7 +105,84 @@ public class GlideinService extends BaseService
 					re.getMessage(),re);
 		}
 	}
+
+	public synchronized void addListener(GlideinListener listener)
+	throws GlideinException
+	{
+		if (listeners.size() == 0) {
+			subscribe();
+		}
+		listeners.add(listener);
+	}
 	
+	public synchronized void removeListener(GlideinListener listener)
+	throws GlideinException
+	{
+		listeners.remove(listener);
+		if (listeners.size() == 0) {
+			unsubscribe();
+		}
+	}
+	
+	private void subscribe() throws GlideinException
+	{
+		try {
+			handler = new GlideinHandler();
+			
+			consumer = NotificationConsumerManager.getInstance();
+			consumer.startListening();
+			
+			consumerEPR = consumer.createNotificationConsumer(handler);
+			
+			TopicExpressionType topicExpression = new TopicExpressionType();
+			topicExpression.setDialect(WSNConstants.SIMPLE_TOPIC_DIALECT);
+			topicExpression.setValue(GlideinNames.TOPIC_STATE_CHANGE);
+			
+			Subscribe subscribe = new Subscribe();
+			subscribe.setUseNotify(Boolean.TRUE);
+			subscribe.setConsumerReference(consumerEPR);
+			subscribe.setTopicExpression(topicExpression);
+	
+			NotificationProducer producer = getProducerPort();
+			
+			producer.subscribe(subscribe);
+		} catch (Exception e) {
+			throw new GlideinException("Unable to subscribe to glidein",e);
+		}
+	}
+	
+	private void unsubscribe() throws GlideinException
+	{
+		try {
+			consumer.removeNotificationConsumer(consumerEPR);
+			consumer.stopListening();
+			consumer = null;
+			consumerEPR = null;
+			handler = null;
+		} catch (Exception e) {
+			throw new GlideinException("Unable to unsubscribe from glidein",e);
+		}
+	}
+	
+	private NotificationProducer getProducerPort() throws GlideinException
+	{
+		try {
+			WSBaseNotificationServiceAddressingLocator locator = 
+				new WSBaseNotificationServiceAddressingLocator();
+			
+			NotificationProducer producer = 
+				locator.getNotificationProducerPort(getEPR());
+			
+			if (getDescriptor() != null) {
+				((Stub)producer)._setProperty(
+						"clientDescriptor", getDescriptor());
+			}
+			
+			return producer;
+		} catch (ServiceException e) {
+			throw new GlideinException("Unable to get notification producer",e);
+		}
+	}
 	private GlideinPortType getPort() throws GlideinException
 	{
 		try {
@@ -100,6 +198,57 @@ public class GlideinService extends BaseService
 		} catch (ServiceException se) {
 			throw new GlideinException("Unable to get port: "+
 					se.getMessage(),se);
+		}
+	}
+	
+	private class GlideinHandler implements NotifyCallback
+	{
+		public void deliver(List list, EndpointReferenceType producer, Object message)
+		{
+			GlideinStateChange stateChange = null;
+			
+			if (message instanceof GlideinStateChangeMessage) {
+				stateChange = ((GlideinStateChangeMessage) message).getGlideinStateChange();
+			} else if (message instanceof GlideinStateChange) {
+				stateChange = (GlideinStateChange)message;
+			} else {
+				try {
+					stateChange = (GlideinStateChange) ObjectDeserializer.toObject(
+							(Element) message, GlideinStateChange.class);
+				} catch (DeserializationException e) {
+					throw new RuntimeException(
+							"Unable to deserialize glidein state change message",e);
+				}
+			}
+			
+			for (GlideinListener listener : listeners) {
+				listener.stateChanged(stateChange);
+			}
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		try {
+			GlideinService s = new GlideinService(
+					new URL("https://juve.usc.edu:8443/wsrf/services/glidein/GlideinService"),5);
+			s.addListener(new GlideinListener(){
+				public void stateChanged(GlideinStateChange stateChange)
+				{
+					System.out.println("State changed: "+stateChange.getState());
+				}
+			});
+			
+			System.out.println("Waiting for notifications");
+			while (true) {
+				try {
+					Thread.sleep(30000);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
--- src/edu/usc/glidein/api/SiteService.java
+++ src/edu/usc/glidein/api/SiteService.java
@@ -17,21 +17,42 @@ package edu.usc.glidein.api;
 
 import java.net.URL;
 import java.rmi.RemoteException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.xml.rpc.ServiceException;
 import javax.xml.rpc.Stub;
 
 import org.apache.axis.message.addressing.EndpointReferenceType;
+import org.globus.wsrf.NotificationConsumerManager;
+import org.globus.wsrf.NotifyCallback;
+import org.globus.wsrf.WSNConstants;
+import org.globus.wsrf.encoding.DeserializationException;
+import org.globus.wsrf.encoding.ObjectDeserializer;
+import org.oasis.wsn.NotificationProducer;
+import org.oasis.wsn.Subscribe;
+import org.oasis.wsn.TopicExpressionType;
+import org.oasis.wsn.WSBaseNotificationServiceAddressingLocator;
+import org.w3c.dom.Element;
 
+import edu.usc.glidein.service.SiteNames;
 import edu.usc.glidein.stubs.RemoveRequest;
 import edu.usc.glidein.stubs.SitePortType;
 import edu.usc.glidein.stubs.service.SiteServiceAddressingLocator;
 import edu.usc.glidein.stubs.types.EmptyObject;
 import edu.usc.glidein.stubs.types.Site;
+import edu.usc.glidein.stubs.types.SiteStateChange;
+import edu.usc.glidein.stubs.types.SiteStateChangeMessage;
 import edu.usc.glidein.util.AddressingUtil;
 
 public class SiteService extends BaseService
-{	
+{
+	private NotificationConsumerManager consumer = null;
+	private EndpointReferenceType consumerEPR = null;
+	private SiteHandler handler = null;
+	private Set<SiteListener> listeners = new HashSet<SiteListener>();
+	
 	public SiteService(EndpointReferenceType epr)
 	{
 		super(epr);
@@ -87,6 +108,84 @@ public class SiteService extends BaseService
 		}
 	}
 	
+	public synchronized void addListener(SiteListener listener)
+	throws GlideinException
+	{
+		if (listeners.size() == 0) {
+			subscribe();
+		}
+		listeners.add(listener);
+	}
+	
+	public synchronized void removeListener(SiteListener listener)
+	throws GlideinException
+	{
+		listeners.remove(listener);
+		if (listeners.size() == 0) {
+			unsubscribe();
+		}
+	}
+	
+	private void subscribe() throws GlideinException
+	{
+		try {
+			handler = new SiteHandler();
+			
+			consumer = NotificationConsumerManager.getInstance();
+			consumer.startListening();
+			
+			consumerEPR = consumer.createNotificationConsumer(handler);
+			
+			TopicExpressionType topicExpression = new TopicExpressionType();
+			topicExpression.setDialect(WSNConstants.SIMPLE_TOPIC_DIALECT);
+			topicExpression.setValue(SiteNames.TOPIC_STATE_CHANGE);
+			
+			Subscribe subscribe = new Subscribe();
+			subscribe.setUseNotify(Boolean.TRUE);
+			subscribe.setConsumerReference(consumerEPR);
+			subscribe.setTopicExpression(topicExpression);
+	
+			NotificationProducer producer = getProducerPort();
+			
+			producer.subscribe(subscribe);
+		} catch (Exception e) {
+			throw new GlideinException("Unable to subscribe to site",e);
+		}
+	}
+	
+	private void unsubscribe() throws GlideinException
+	{
+		try {
+			consumer.removeNotificationConsumer(consumerEPR);
+			consumer.stopListening();
+			consumer = null;
+			consumerEPR = null;
+			handler = null;
+		} catch (Exception e) {
+			throw new GlideinException("Unable to unsubscribe from site",e);
+		}
+	}
+	
+	private NotificationProducer getProducerPort() throws GlideinException
+	{
+		try {
+			WSBaseNotificationServiceAddressingLocator locator = 
+				new WSBaseNotificationServiceAddressingLocator();
+			
+			NotificationProducer producer = 
+				locator.getNotificationProducerPort(getEPR());
+			
+			if (getDescriptor() != null) {
+				((Stub)producer)._setProperty(
+						"clientDescriptor", getDescriptor());
+			}
+			
+			return producer;
+		} catch (ServiceException e) {
+			throw new GlideinException("Unable to get notification producer",e);
+		}
+	}
+	
 	private SitePortType getPort() throws GlideinException
 	{
 		try {
@@ -102,6 +201,57 @@ public class SiteService extends BaseService
 		} catch (ServiceException se) {
 			throw new GlideinException("Unable to get SitePortType: "+
 					se.getMessage(),se);
+		}
+	}
+	
+	private class SiteHandler implements NotifyCallback
+	{
+		public void deliver(List list, EndpointReferenceType producer, Object message)
+		{
+			SiteStateChange stateChange = null;
+			
+			if (message instanceof SiteStateChangeMessage) {
+				stateChange = ((SiteStateChangeMessage) message).getSiteStateChange();
+			} else if (message instanceof SiteStateChange) {
+				stateChange = (SiteStateChange)message;
+			} else {
+				try {
+					stateChange = (SiteStateChange) ObjectDeserializer.toObject(
+							(Element) message, SiteStateChange.class);
+				} catch (DeserializationException e) {
+					throw new RuntimeException(
+							"Unable to deserialize site state change message",e);
+				}
+			}
+			
+			for (SiteListener listener : listeners) {
+				listener.stateChanged(stateChange);
+			}
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		try {
+			SiteService s = new SiteService(
+					new URL("https://juve.usc.edu:8443/wsrf/services/glidein/SiteService"),13);
+			s.addListener(new SiteListener(){
+				public void stateChanged(SiteStateChange stateChange)
+				{
+					System.out.println("State changed: "+stateChange.getState());
+				}
+			});
+			
+			System.out.println("Waiting for notifications");
+			while (true) {
+				try {
+					Thread.sleep(30000);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
