9a2a9e217efb754fe5aa36bfa7d41525862e343b
Gideon Juve <juve@usc.edu>
2009-02-12 00:14:59 +0000

    Update web pages for 1.0.7
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1641 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- web/download.php
+++ web/download.php
@@ -7,7 +7,7 @@ include_once("../../includes/header.inc");
 
 <h2>Binary Releases</h2>
 
-<p>The latest stable release is <b>1.0.6</b>. Click <a href="..">here</a> for older releases.</p>
+<p>The latest stable release is <b>1.0.7</b>. Click <a href="..">here</a> for older releases.</p>
 
 <h3>Service and Client</h3>
 <ul><li><a href="glidein_service.gar">glidein_service.gar</a><br>Download this if you want both the service and the client</li></ul>
