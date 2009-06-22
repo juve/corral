abfddd5468bfb45fd0a5417b1f61257c29a94e3f
Gideon Juve <juve@usc.edu>
2009-06-22 22:20:41 +0000

    No more InitializationException
    git-svn-id: https://pegasus.isi.edu/svn/corral/trunk@1792 e217846f-e12e-0410-a4e5-89ccaea66ff7

--- src/edu/usc/glidein/service/GlideinResourceHome.java
+++ src/edu/usc/glidein/service/GlideinResourceHome.java
@@ -24,7 +24,6 @@ import org.globus.wsrf.ResourceException;
 import org.globus.wsrf.ResourceKey;
 import org.globus.wsrf.impl.ResourceHomeImpl;
 import org.globus.wsrf.impl.SimpleResourceKey;
-import org.globus.wsrf.impl.security.authorization.exceptions.InitializeException;
 
 import edu.usc.glidein.db.Database;
 import edu.usc.glidein.db.DatabaseException;
@@ -61,7 +60,7 @@ public class GlideinResourceHome extends ResourceHomeImpl
 				resource.recoverState();
 			}
 		} catch (DatabaseException de) {
-			throw new InitializeException(
+			throw new Exception(
 					"Unable to get glideins from database",de);
 		}
 		
